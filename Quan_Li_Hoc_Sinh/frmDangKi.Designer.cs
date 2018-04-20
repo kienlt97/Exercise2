cbdf76f94d3afb925841764deaa0e28cd9c547c1
Dang Bao <dangbaoxx@gmail.com>
2018-04-20 20:28:00 +0700

    Cập nhật xong form đăng ký tài khoản

--- Quan_Li_Hoc_Sinh/frmDangKi.Designer.cs
+++ Quan_Li_Hoc_Sinh/frmDangKi.Designer.cs
@@ -29,32 +29,32 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.txbUsername = new System.Windows.Forms.TextBox();
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.btnDangky = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.txtConfirmPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnDangky);
             this.panel1.Controls.Add(this.panel5);
             this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.panel3);
@@ -65,74 +65,69 @@
             this.panel1.Size = new System.Drawing.Size(717, 530);
             this.panel1.TabIndex = 4;
             // 
-            // panel2
+            // label3
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
-            this.panel2.Controls.Add(this.txbUsername);
-            this.panel2.Controls.Add(this.lblUsername);
-            this.panel2.Location = new System.Drawing.Point(62, 95);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(592, 53);
-            this.panel2.TabIndex = 4;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(191, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(344, 33);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nhập thông tin tài khoản";
             // 
-            // txbUsername
+            // btnDangky
             // 
-            this.txbUsername.Location = new System.Drawing.Point(212, 14);
-            this.txbUsername.Name = "txbUsername";
-            this.txbUsername.Size = new System.Drawing.Size(323, 22);
-            this.txbUsername.TabIndex = 1;
+            this.btnDangky.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.btnDangky.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangky.Location = new System.Drawing.Point(485, 452);
+            this.btnDangky.Name = "btnDangky";
+            this.btnDangky.Size = new System.Drawing.Size(112, 42);
+            this.btnDangky.TabIndex = 8;
+            this.btnDangky.Text = "Đăng ký";
+            this.btnDangky.UseVisualStyleBackColor = true;
+            this.btnDangky.Click += new System.EventHandler(this.btnDangky_Click);
             // 
-            // lblUsername
+            // panel5
             // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.Location = new System.Drawing.Point(15, 11);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(152, 24);
-            this.lblUsername.TabIndex = 0;
-            this.lblUsername.Text = "Tên đăng nhập";
+            this.panel5.Controls.Add(this.txtConfirmPass);
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Location = new System.Drawing.Point(62, 359);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(592, 53);
+            this.panel5.TabIndex = 7;
             // 
-            // panel3
+            // txtConfirmPass
             // 
-            this.panel3.Controls.Add(this.textBox2);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(62, 180);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(592, 53);
-            this.panel3.TabIndex = 5;
+            this.txtConfirmPass.Location = new System.Drawing.Point(212, 14);
+            this.txtConfirmPass.Name = "txtConfirmPass";
+            this.txtConfirmPass.Size = new System.Drawing.Size(323, 22);
+            this.txtConfirmPass.TabIndex = 1;
             // 
-            // textBox2
+            // label4
             // 
-            this.textBox2.Location = new System.Drawing.Point(212, 14);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(323, 22);
-            this.textBox2.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(15, 14);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 24);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Tên hiển thị";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nhập lại mật khẩu";
             // 
             // panel4
             // 
-            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.txtPassword);
             this.panel4.Controls.Add(this.label1);
             this.panel4.Location = new System.Drawing.Point(62, 269);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(592, 53);
             this.panel4.TabIndex = 6;
             // 
-            // textBox1
+            // txtPassword
             // 
-            this.textBox1.Location = new System.Drawing.Point(212, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(323, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtPassword.Location = new System.Drawing.Point(212, 14);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(323, 22);
+            this.txtPassword.TabIndex = 1;
             // 
             // label1
             // 
@@ -145,52 +140,58 @@
             this.label1.Text = "Mật khẩu";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // panel5
+            // panel3
             // 
-            this.panel5.Controls.Add(this.textBox4);
-            this.panel5.Controls.Add(this.label4);
-            this.panel5.Location = new System.Drawing.Point(62, 359);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(592, 53);
-            this.panel5.TabIndex = 7;
+            this.panel3.Controls.Add(this.txtTen);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(62, 180);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(592, 53);
+            this.panel3.TabIndex = 5;
             // 
-            // textBox4
+            // txtTen
             // 
-            this.textBox4.Location = new System.Drawing.Point(212, 14);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(323, 22);
-            this.textBox4.TabIndex = 1;
+            this.txtTen.Location = new System.Drawing.Point(212, 14);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(323, 22);
+            this.txtTen.TabIndex = 1;
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(15, 14);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(179, 24);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Nhập lại mật khẩu";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên hiển thị";
             // 
-            // button1
+            // panel2
             // 
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
-            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(485, 452);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 42);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Đăng ký";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Controls.Add(this.txtUsername);
+            this.panel2.Controls.Add(this.lblUsername);
+            this.panel2.Location = new System.Drawing.Point(62, 95);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(592, 53);
+            this.panel2.TabIndex = 4;
             // 
-            // label3
+            // txtUsername
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(191, 23);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(344, 33);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Nhập thông tin tài khoản";
+            this.txtUsername.Location = new System.Drawing.Point(212, 14);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(323, 22);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(15, 11);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(152, 24);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Tên đăng nhập";
             // 
             // frmDangKi
             // 
@@ -202,16 +203,17 @@
             this.Name = "frmDangKi";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tạo tài khoản";
+            this.Load += new System.EventHandler(this.frmDangKi_Load_1);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -219,18 +221,18 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDangky;
         private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtConfirmPass;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtTen;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.TextBox txbUsername;
+        private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.Label label3;
     }
--- Quan_Li_Hoc_Sinh/frmDangKi.cs
+++ Quan_Li_Hoc_Sinh/frmDangKi.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Quan_Li_Hoc_Sinh
 {
@@ -16,10 +17,65 @@ namespace Quan_Li_Hoc_Sinh
         {
             InitializeComponent();
         }
+        DataConnection dt = new DataConnection();
+        List<string> lst = new List<string>();
+        public void PullData()
+        {
+            dt.OpenConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Login";
+            cmd.Connection = dt.conn;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string username = reader.GetString(0);
+                lst.Add(username);
+            }
+            reader.Close();
+        }
+       
+        public void AddTK_Database()
+        {
+            dt.OpenConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "ThemTK";
+            cmd.Connection = dt.conn;
+
+            cmd.Parameters.Add("@USERNAME", SqlDbType.NVarChar).Value = txtUsername.Text;
+            cmd.Parameters.Add("@PASS", SqlDbType.NVarChar).Value = txtPassword.Text;
+            int ret = cmd.ExecuteNonQuery();
+        }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void frmDangKi_Load_1(object sender, EventArgs e)
+        {
+            PullData();
+        }
+        bool check = true;
+        private void btnDangky_Click(object sender, EventArgs e)
+        {
+            foreach (string us in lst)
+            {
+                if (us.Contains(txtUsername.Text) || txtPassword.Text != txtConfirmPass.Text)
+                {
+                    MessageBox.Show("Tên tài khoản đã tồn tại hoặc mật khẩu không khớp !");
+                    check = false;
+                    break;
+                }
+                check = true;
+            }
+            if (check == true)
+            {
+                AddTK_Database();
+                MessageBox.Show("Đăng ký thành công!");
+            }
         }
     }
 }
